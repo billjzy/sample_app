d3e8bf27c58572cead5a411119229057fd0e8f71
Zhangyi Jin <billjzy637@gmail.com>
2015-01-15 11:19:30 -0500

    log-in-log-out

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'signup' => 'users#new'
   resources :users
   root  'static_pages#home'
-  get 'login' => 'static_pages#login'
+  get 'login' => 'sessions#new'
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
